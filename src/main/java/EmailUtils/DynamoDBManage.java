54921cc58bdae8d19129fd703317344fa113510d
Mengchen Gao <gao.mengc@husky.neu.edu>
2020-04-03 16:23:56 -0400

    test

--- src/main/java/EmailUtils/DynamoDBManage.java
+++ src/main/java/EmailUtils/DynamoDBManage.java
@@ -8,13 +8,10 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
-import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
-import com.amazonaws.services.dynamodbv2.model.ReturnValue;
 import com.amazonaws.services.lambda.runtime.Context;
 
 public class DynamoDBManage {
 
-    private int DURATION;
     private AmazonDynamoDB dynamoDBClient;
     private String TABLE_NAME = System.getenv("DYNAMODB_NAME");
     private String PR_KEY = System.getenv("DYNAMODB_PR_KEY");
@@ -35,30 +32,10 @@ public class DynamoDBManage {
 
         try{
             Item userToken = new Item().withPrimaryKey(PR_KEY, username)
-                    .withLong("Time", System.currentTimeMillis());
+                    .withNumber("ExpireTTL",(System.currentTimeMillis() + GAP)/ 1000L );
             table.putItem(userToken);
         }catch (Exception e){
             context.getLogger().log("Create items failed.");
-            context.getLogger().log(e.getMessage());
-        }
-    }
-
-    public void deleteToken(){
-
-        Table table = new DynamoDB(dynamoDBClient).getTable(TABLE_NAME);
-        try {
-
-            DeleteItemSpec deleteItemSpec = new DeleteItemSpec().withPrimaryKey(PR_KEY, this.username);
-
-            DeleteItemOutcome outcome = table.deleteItem(deleteItemSpec);
-
-            // Check the response.
-            context.getLogger().log("Printing item that was deleted...");
-            context.getLogger().log(outcome.getItem().toJSONPretty());
-
-        }
-        catch (Exception e) {
-            context.getLogger().log("Error deleting item in " + this.TABLE_NAME);
             context.getLogger().log(e.getMessage());
         }
     }
@@ -72,23 +49,22 @@ public class DynamoDBManage {
         context.getLogger().log("PR_KEY Name " + PR_KEY);
 
         Table table = new DynamoDB(dynamoDBClient).getTable(TABLE_NAME);
+
+        context.getLogger().log("table " + table);
+
         this.username = username;
+
         try {
             Item item = table.getItem(PR_KEY, username);
             if(item == null){
                 createToken();
                 return true;
-            }
-
-            context.getLogger().log("Printing item after retrieving it....");
-            context.getLogger().log(item.toJSONPretty());
-
-            if((System.currentTimeMillis() - item.getLong("Time")) >= GAP){
-                deleteToken();
-                return true;
             }else{
+                context.getLogger().log("Printing item after retrieving it....");
+                context.getLogger().log(item.toJSONPretty());
                 return false;
             }
+
         } catch (Exception e) {
             context.getLogger().log("GetItem failed.");
             context.getLogger().log(e.getMessage());
